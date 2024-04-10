5a3603094f8d8b7170f713a9bf2b638656e64fc6
uwezukwechibuzor <uwezukwechibuzor@gmail.com>
2024-04-10 21:53:30 +0100

    btchandshakeCmd

---
+++ cmd/btchandshake.go
@@ -0,0 +1,55 @@
+package cmd
+
+import (
+	"os"
+
+	"github.com/sirupsen/logrus"
+	"github.com/spf13/cobra"
+	"github.com/uwezukwechibuzor/bitcoin-node-handshake/node"
+	"github.com/uwezukwechibuzor/bitcoin-node-handshake/rpc"
+)
+
+const (
+	userAgent = "/Satoshi:5.64/tinybit:0.0.1/"
+)
+
+var (
+	network     string
+	nodeAddr    string
+	jsonrpcPort int
+)
+
+func init() {
+	btchandshakeCmd.Flags().IntVar(&jsonrpcPort, "jsonrpc-port", 9334, "Port to listen JSON-RPC connections on")
+	btchandshakeCmd.Flags().StringVar(&nodeAddr, "node-addr", "127.0.0.1:9333", "TCP address of a Bitcoin node to connect to")
+	btchandshakeCmd.Flags().StringVar(&network, "network", "simnet", "Bitcoin network (simnet, mainnet)")
+}
+
+var btchandshakeCmd = &cobra.Command{
+	Use: "btchandshake",
+	RunE: func(cmd *cobra.Command, args []string) error {
+		node, err := node.New(network, userAgent)
+		if err != nil {
+			return err
+		}
+
+		rpc, err := rpc.NewServer(jsonrpcPort, node)
+		if err != nil {
+			return err
+		}
+
+		logrus.Infof("Running JSON-RPC server on port %d", jsonrpcPort)
+		go rpc.Run()
+
+		return node.Run(nodeAddr)
+	},
+}
+
+// Execute
+func Execute() {
+
+	if err := btchandshakeCmd.Execute(); err != nil {
+		logrus.Fatalln(err)
+		os.Exit(1)
+	}
+}
--- go.mod
+++ go.mod
@@ -5,9 +5,12 @@ go 1.21.6
 require (
 	github.com/google/go-cmp v0.6.0
 	github.com/sirupsen/logrus v1.9.3
+	github.com/spf13/cobra v1.8.0
 )
 
 require (
+	github.com/inconshreveable/mousetrap v1.1.0 // indirect
+	github.com/spf13/pflag v1.0.5 // indirect
 	github.com/stretchr/testify v1.8.1 // indirect
 	golang.org/x/sys v0.0.0-20220715151400-c0bba94af5f8 // indirect
 )
